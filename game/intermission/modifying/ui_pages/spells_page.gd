f482146db843472e14db855c3e88247bbf8fc0ef
Luicel <luicelcs@gmail.com>
2024-12-19 02:37:30 -0600

    finish modifying state spells page

--- game/intermission/modifying/ui_pages/spells_page.gd
+++ game/intermission/modifying/ui_pages/spells_page.gd
@@ -3,7 +3,6 @@ extends VBoxContainer
 signal readied
 
 var player: Player
-var item_list_spell_slot: int
 var item_spell_types: Array[Spells.Type]
 
 
@@ -12,6 +11,7 @@ func setup(player: Player):
 	
 	_update_current_spells_ui()
 	_populate_spell_list(1)
+	_update_select_spell_slot_buttons()
 
 
 func _update_current_spells_ui():
@@ -40,10 +40,11 @@ func _update_current_spells_ui():
 
 
 func _populate_spell_list(spell_slot: int):
-	item_list_spell_slot = spell_slot
-	
 	%SpellsList.clear()
 	item_spell_types.clear()
+	
+	%SpellsList.add_item("None")
+	item_spell_types.append(Spells.Type.NONE)
 	for spell_type in Spells.Type.values():
 		if player.spell_inventory.has(spell_type):
 			var spell_resource = Spells.get_spell_resource(spell_type)
@@ -56,31 +57,16 @@ func _populate_spell_list(spell_slot: int):
 			var is_selectable = true #not player.spell_inventory.equipped_spell_types.has(spell_type)
 			%SpellsList.add_item(text, spell_icon, is_selectable)
 			
-			if spell_type == player.spell_inventory.equipped_spell_types[spell_slot - 1]:
-				%SpellsList.select(%SpellsList.get_item_count() - 1)
-			
 			item_spell_types.append(spell_type)
 
 
 func _on_spells_list_item_selected(index: int) -> void:
-	var spell_slot = item_list_spell_slot
 	var selected_spell = item_spell_types[index]
-	_select_new_equipped_spell(spell_slot, selected_spell)
-	
 	var spell_resource = Spells.get_spell_resource(selected_spell)
 	var spell_level = player.spell_inventory.get_level(selected_spell)
 	_set_spell_description(spell_resource, spell_level)
-
-
-func _select_new_equipped_spell(spell_slot: int, selected_spell: Spells.Type):
-	for i in range(player.spell_inventory.equipped_spell_types.size()):
-		var checked_spell = player.spell_inventory.equipped_spell_types[i]
-		if selected_spell == checked_spell:
-			player.spell_inventory.set_spell_slot.rpc(i, Spells.Type.NONE)
 	
-	player.spell_inventory.set_spell_slot.rpc(spell_slot - 1, selected_spell)
-	_update_current_spells_ui()
-	_populate_spell_list(spell_slot)
+	_update_select_spell_slot_buttons()
 
 
 func _set_spell_description(spell_resource: Spell, spell_level: int):
@@ -96,6 +82,50 @@ func _set_spell_description(spell_resource: Spell, spell_level: int):
 
 func _clear_spell_description():
 	%SpellDescriptionContainer.visibility_layer = 0
+
+
+func _on_select_spell_button_pressed(spell_slot: int) -> void:
+	var selected_spell = _get_list_selected_spell()
+	_select_new_equipped_spell(spell_slot, selected_spell)
+
+
+func _select_new_equipped_spell(spell_slot: int, selected_spell: Spells.Type):
+	for i in range(player.spell_inventory.equipped_spell_types.size()):
+		var checked_spell = player.spell_inventory.equipped_spell_types[i]
+		if selected_spell == checked_spell:
+			player.spell_inventory.set_spell_slot.rpc_id(player.peer_id, i, Spells.Type.NONE)
+	
+	player.spell_inventory.set_spell_slot.rpc(spell_slot - 1, selected_spell)
+	_update_current_spells_ui()
+	_update_select_spell_slot_buttons()
+
+
+func _update_select_spell_slot_buttons():
+	var select_spell_slot_buttons = [
+		%SelectSpellSlot1Button, %SelectSpellSlot2Button, %SelectSpellSlot3Button
+	]
+	
+	for i in range(player.spell_inventory.equipped_spell_types.size()):
+		var select_spell_slot_button = select_spell_slot_buttons[i]
+		if %SpellsList.get_selected_items().size() > 0:
+			var equipped_spell_type = player.spell_inventory.equipped_spell_types[i]
+			var selected_spell_type = _get_list_selected_spell()
+			select_spell_slot_button.disabled = equipped_spell_type == selected_spell_type
+		else:
+			select_spell_slot_button.disabled = true
+
+
+func _get_list_selected_spell() -> Spells.Type:
+	if %SpellsList.get_selected_items().size() == 0: return Spells.Type.NONE
+	
+	for spell_type in Spells.Type.values():
+		var item_spell_types_index = item_spell_types.find(spell_type, 0)
+		if item_spell_types_index == -1: 
+			continue
+		elif %SpellsList.is_selected(item_spell_types_index) == true:
+			return spell_type
+	
+	return Spells.Type.NONE
 
 
 func _on_ready_button_pressed() -> void:
