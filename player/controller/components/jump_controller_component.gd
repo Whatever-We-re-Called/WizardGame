cdedf0c01a200c0673aca593c6a2bbb6648c1d5c
Luicel <luicelcs@gmail.com>
2025-01-07 02:08:27 -0600

    add wall jump coyote time

--- player/controller/components/jump_controller_component.gd
+++ player/controller/components/jump_controller_component.gd
@@ -3,39 +3,51 @@ extends Node
 @export var jump_strength: float
 @export var wall_jump_strength: Vector2
 @export var jump_buffer_time: float
-@export var coyote_time: float
+@export var jump_coyote_time: float
+@export var wall_jump_coyote_time: float
 
 @onready var jump_buffer_timer = TimerUtil.create_basic_timer(self, jump_buffer_time)
-@onready var coyote_timer = TimerUtil.create_basic_timer(self, coyote_time)
+@onready var jump_coyote_timer = TimerUtil.create_basic_timer(self, jump_coyote_time)
+@onready var wall_jump_coyote_timer = TimerUtil.create_basic_timer(self, wall_jump_coyote_time)
 
 var is_going_up: bool = false
 var is_jumping: bool = false
+var is_wall_jumping: bool = false
 var was_on_floor_last_frame: bool = false
+var was_on_wall_only_last_frame: bool = false
 
 
 func handle_jump(body: CharacterBody2D, wants_to_jump: bool, released_jump: bool):
 	if _has_just_landed(body):
 		is_jumping = false
+	if _has_just_touched_wall(body):
+		is_wall_jumping = false
 	
 	if _is_allowed_to_jump(body, wants_to_jump):
 		_execute_jump(body)
-	
-	_try_to_wall_jump(body, wants_to_jump)
-	
-	_handle_coyote_time(body)
+	_handle_jump_coyote_timer(body)
 	_handle_jump_buffer(body, wants_to_jump)
 	_handle_variable_jump_height(body, released_jump)
 	
+	if _is_allowed_to_wall_jump(body, wants_to_jump):
+		_execute_wall_jump(body)
+	_handle_wall_jump_coyote_timer(body)
+	
 	is_going_up = body.velocity.y < 0 and not body.is_on_floor()
 	was_on_floor_last_frame = body.is_on_floor()
+	was_on_wall_only_last_frame = body.is_on_wall_only()
 
 
 func _has_just_landed(body: CharacterBody2D) -> bool:
 	return body.is_on_floor() and not was_on_floor_last_frame and is_jumping
 
 
+func _has_just_touched_wall(body: CharacterBody2D) -> bool:
+	return body.is_on_wall_only() and not was_on_wall_only_last_frame and is_wall_jumping
+
+
 func _is_allowed_to_jump(body: CharacterBody2D, wants_to_jump: bool) -> bool:
-	return wants_to_jump and (body.is_on_floor() or not coyote_timer.is_stopped())
+	return wants_to_jump and (body.is_on_floor() or not jump_coyote_timer.is_stopped())
 
 
 func _execute_jump(body: CharacterBody2D):
@@ -43,21 +55,12 @@ func _execute_jump(body: CharacterBody2D):
 	body.velocity.y = jump_strength
 	
 	jump_buffer_timer.stop()
-	coyote_timer.stop()
+	jump_coyote_timer.stop()
 
 
-func _try_to_wall_jump(body: CharacterBody2D, wants_to_jump: bool):
-	if body.is_on_wall_only() and wants_to_jump:
-		var wall_normal = body.get_wall_normal()
-		body.velocity = Vector2(
-			wall_jump_strength.x * -wall_normal.x,
-			wall_jump_strength.y
-		)
-
-
-func _handle_coyote_time(body: CharacterBody2D):
+func _handle_jump_coyote_timer(body: CharacterBody2D):
 	if _has_just_stepped_off_ledge(body):
-		coyote_timer.start()
+		jump_coyote_timer.start()
 
 
 func _has_just_stepped_off_ledge(body: CharacterBody2D) -> bool:
@@ -75,3 +78,26 @@ func _handle_jump_buffer(body: CharacterBody2D, wants_to_jump: bool):
 func _handle_variable_jump_height(body: CharacterBody2D, released_jump: bool):
 	if released_jump and is_going_up:
 		body.velocity.y /= 2.0
+
+
+func _is_allowed_to_wall_jump(body: CharacterBody2D, wants_to_jump: bool) -> bool:
+	return wants_to_jump and (body.is_on_wall_only() or not wall_jump_coyote_timer.is_stopped())
+
+
+func _execute_wall_jump(body: CharacterBody2D):
+	is_wall_jumping = true
+	body.velocity = Vector2(
+		wall_jump_strength.x * -body.get_wall_normal().x,
+		wall_jump_strength.y
+	)
+	
+	wall_jump_coyote_timer.stop()
+
+
+func _handle_wall_jump_coyote_timer(body: CharacterBody2D):
+	if _has_just_left_wall(body):
+		wall_jump_coyote_timer.start()
+
+
+func _has_just_left_wall(body: CharacterBody2D) -> bool:
+	return not body.is_on_wall() and was_on_wall_only_last_frame and not is_wall_jumping
