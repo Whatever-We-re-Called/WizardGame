4f26e8271339810b2bd88e9e17687171a6a006ea
Luicel <luicelcs@gmail.com>
2025-01-06 22:45:21 -0600

    add terminal gravity velocity

--- player/controller/components/gravity_controller_component.gd
+++ player/controller/components/gravity_controller_component.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var climb_gravity: float
 @export var fall_gravity: float
+@export var terminal_gravity: float
 
 var is_falling: bool = false
 var prevent_jump: bool = false
@@ -15,7 +16,11 @@ func handle_gravity(body: CharacterBody2D, delta: float):
 
 func _apply_gravity(body: CharacterBody2D, delta: float):
 	if body.is_on_floor() == false:
+		var new_y_velocity: float
 		if is_falling == true:
-			body.velocity.y += fall_gravity * delta
+			new_y_velocity = body.velocity.y + fall_gravity * delta
 		else:
-			body.velocity.y += climb_gravity * delta
+			new_y_velocity = body.velocity.y + climb_gravity * delta
+		new_y_velocity = min(new_y_velocity, terminal_gravity)
+		
+		body.velocity.y = new_y_velocity
